37036f83ecf7a76c749a84635b3cd59169511378
xzMatheuSx <matheusaugusth@gmail.com>
2025-04-02 18:14:21 -0300

    correção de bugs

--- app/src/main/java/com/example/tourgemeas/Tela_principal.kt
+++ app/src/main/java/com/example/tourgemeas/Tela_principal.kt
@@ -2,6 +2,7 @@ package com.example.tourgemeas
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
