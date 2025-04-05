8d613f315c80cd01828b0749b87905dd4a565cdc
xzMatheuSx <matheusaugusth@gmail.com>
2025-04-04 21:19:10 -0300

    corrigindo  bugs

--- tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/TelaPrincipalActivity.kt
+++ tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/TelaPrincipalActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Tela_principal : AppCompatActivity() {
+class TelaPrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
