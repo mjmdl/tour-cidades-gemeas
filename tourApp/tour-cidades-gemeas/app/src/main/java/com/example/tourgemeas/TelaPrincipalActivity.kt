e3c4ca4280bebe9c5816a7cec5a67e80f5c1dd93
xzMatheuSx <matheusaugusth@gmail.com>
2025-04-05 22:32:59 -0300

    add menu navegação

--- tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/TelaPrincipalActivity.kt
+++ tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/TelaPrincipalActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
 
 class TelaPrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_principal)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("Perfil"))
+        tabLayout.addTab(tabLayout.newTab().setText("Mapa"))
+        tabLayout.addTab(tabLayout.newTab().setText("Enigma"))
+        tabLayout.addTab(tabLayout.newTab().setText("Classificação"))
+
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                // abas navegacao
+                when (tab.position) {//*inicia navegacao */ }
+                    0 -> { }
+
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
     }
 }
