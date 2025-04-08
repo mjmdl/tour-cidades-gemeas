8b419054cc979b2aa50628be75fec28f11512285
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2025-04-08 19:50:55 -0300

    Da tela inicial para a de login

--- tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/LoguinActivity.kt
+++ tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/LoguinActivity.kt
@@ -1,20 +1,29 @@
 package com.example.tourgemeas
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tourgemeas.databinding.ActivityLoguinBinding
 
 class LoguinActivity : AppCompatActivity() {
+
+    private lateinit var telaLoguinAct: ActivityLoguinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        telaLoguinAct = ActivityLoguinBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_loguin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(telaLoguinAct.root)
+
+        telaLoguinAct.btnInicialSessao.setOnClickListener() {
+            val intent = Intent(this, TelaPrincipalActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
--- tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/MainActivity.kt
+++ tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.tourgemeas
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        lifecycleScope.launch {
+            delay(2000)
+            val intent = Intent(this@MainActivity,
+                LoguinActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
