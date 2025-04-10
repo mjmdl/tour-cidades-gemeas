93d78b9a9defd78bd97d9dbafeb07cdb792246cf
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2025-04-09 22:11:08 -0300

    Mapa pegando localização Atual

--- tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/TelaPrincipalActivity.kt
+++ tourApp/tour-cidades-gemeas/app/src/main/java/com/example/tourgemeas/TelaPrincipalActivity.kt
@@ -1,17 +1,43 @@
 package com.example.tourgemeas
 
+
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.LayoutInflater
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.tabs.TabLayout
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import android.Manifest
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class TelaPrincipalActivity : AppCompatActivity() {
+
+    private lateinit var map: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_principal)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -19,27 +45,59 @@ class TelaPrincipalActivity : AppCompatActivity() {
             insets
         }
 
-
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-
 
         tabLayout.addTab(tabLayout.newTab().setText("Perfil"))
         tabLayout.addTab(tabLayout.newTab().setText("Mapa"))
         tabLayout.addTab(tabLayout.newTab().setText("Enigma"))
         tabLayout.addTab(tabLayout.newTab().setText("Classificação"))
 
-
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                // abas navegacao
-                when (tab.position) {//*inicia navegacao */ }
-                    0 -> { }
+                when (tab.position) {
+                    0 -> {
+                        // Outras abas
+                    }
+                    1 -> {
+                        val inflater = LayoutInflater.from(this@TelaPrincipalActivity)
+                        val mapView = inflater.inflate(R.layout.fragment_mapa, null)
+                        val frame = findViewById<FrameLayout>(R.id.content_frame)
+                        frame.removeAllViews()
+                        frame.addView(mapView)
 
+                        map = mapView.findViewById(R.id.map)
+                        Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
+
+                        map.setTileSource(TileSourceFactory.MAPNIK)
+                        map.setMultiTouchControls(true)
+
+                        val mapController = map.controller
+                        mapController.setZoom(18.0)
+
+                        val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this@TelaPrincipalActivity), map)
+                        locationOverlay.enableMyLocation()
+                        locationOverlay.enableFollowLocation()
+                        map.overlays.add(locationOverlay)
+                    }
                 }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::map.isInitialized) {
+            map.onResume()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (::map.isInitialized) {
+            map.onPause()
+        }
     }
 }
